af68ff1bb8ee8721dce103c53c294a604a209bb3
YoanFay <yoanfayolle.yf@gmail.com>
2026-01-30 16:05:06 +0100

    feat(short): add short version

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ const appPort = 3000
 const data = await readFile('./public/countries.json');
 const json = JSON.parse(data);
 
-app.get('/', (req, res) => {
+function normalVersion() {
 
     let countries = []
 
     for(let country in json){
         countries.push(
             {
-            'name': json[country].name??null,
+            'name': json[country].name.common??null,
             'cca2': json[country].cca2??null,
             'cca3': json[country].cca3??null,
             'currencies': json[country].currencies??null,
@@ -28,13 +28,42 @@ app.get('/', (req, res) => {
         )        
     }
 
-    res.send(countries)
+    return countries
+    
+}
+
+function shortVersion() {
+
+    let countries = []
+
+    for(let country in json){
+        countries.push(
+            {
+            'name': json[country].name.common??null,
+            'cca2': json[country].cca2??null,
+            'cca3': json[country].cca3??null,
+            'flag': json[country].flag??null,
+            }
+        )        
+    }
+
+    return countries
+    
+}
+
+app.get('/', (req, res) => {
+
+    res.send(normalVersion())
     
 })
 
 app.get('/:version', (req, res) => {
     if(req.params.version == "full"){
         res.send(json)
+    }else if(req.params.version == "short"){
+        res.send(shortVersion())
+    }else if(req.params.version == "normal"){
+        res.send(shortVersion())
     }
 })
 
