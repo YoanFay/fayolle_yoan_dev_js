5f5406ac179db05faec0960034a51c526417a29a
YoanFay <yoanfayolle.yf@gmail.com>
2026-01-30 15:54:41 +0100

    feat(normal): add normal version

--- src/main.js
+++ src/main.js
@@ -9,8 +9,33 @@ const data = await readFile('./public/countries.json');
 const json = JSON.parse(data);
 
 app.get('/', (req, res) => {
-    console.log('req')
-    res.send(json)
+
+    let countries = []
+
+    for(let country in json){
+        countries.push(
+            {
+            'name': json[country].name??null,
+            'cca2': json[country].cca2??null,
+            'cca3': json[country].cca3??null,
+            'currencies': json[country].currencies??null,
+            'languages': json[country].languages??null,
+            'flag': json[country].flag??null,
+            'capital': json[country].capital??null,
+            'population': json[country].population??null,
+            'continents': json[country].continents??null,
+            }
+        )        
+    }
+
+    res.send(countries)
+    
+})
+
+app.get('/:version', (req, res) => {
+    if(req.params.version == "full"){
+        res.send(json)
+    }
 })
 
 app.listen(appPort, () => {
