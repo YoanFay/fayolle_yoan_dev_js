9ff53ae4d7d0cbbb28396d3eeb60f544c7ea4d04
YoanFay <yoanfayolle.yf@gmail.com>
2026-01-30 15:36:49 +0100

    feat(full): add full version

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import { readFile } from 'fs/promises';
 
 const app = express();
 
 const appPort = 3000
 
+const data = await readFile('./public/countries.json');
+const json = JSON.parse(data);
+
 app.get('/', (req, res) => {
+    console.log('req')
+    res.send(json)
 })
 
 app.listen(appPort, () => {
-    console.log(`app launched on port ${appPort}`)
+    console.log(`app launched on port ${appPort}`);
 })
